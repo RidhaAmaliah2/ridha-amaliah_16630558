16a01adc71a002fa8544003c83301da04b0f2138
RidhaAmaliah2 <ridhaamaliah3@gmail.com>
2021-03-14 16:48:13 +0800

    commit pertama project praktikum Ridha Amaliah NPM 16630558

---
+++ profile.php
@@ -0,0 +1,44 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/RIDHA.jpg" alt="RIDHA AMALIAH" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">16630558</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Ridha Amaliah</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Lasung Batu, 19 Oktober 1998</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0822 5064 8518</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">ridhaamaliah3@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                        
+                            <a href="http://www.instagram.com/ridha.amaliah" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/029-instagram.svg" width="30" alt="instagram">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
